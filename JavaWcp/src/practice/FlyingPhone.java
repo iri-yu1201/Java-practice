5322bcf6acb2a5d1257d1a994fda7c9d698c252c
iri-yu1201 <iri12yuta01@gmail.com>
2021-09-09 14:36:36 +0900

    chapter14 polymorphism

--- JavaWcp/src/practice/FlyingPhone.java
+++ JavaWcp/src/practice/FlyingPhone.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class FlyingPhone {
+import extend.phone.Phone;
 
+public class FlyingPhone implements Phone, Flying {
+	private int minutes;
+	
+	public FlyingPhone(int minutes) {
+		this.minutes = minutes;
+	}
+	
+	public void call(String number) {
+		System.out.println(number + "に通話します。通話できるのは飛んでいる間だけです。");
+	}
+	
+	public void fly() {
+		System.out.println(minutes + "分間、飛びます。");
+	}
 }
